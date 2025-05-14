18f98e3b8b28d50ca3628720fa6b639b045d2b12
MirzaDhanuSulistyo <35160617+MirzaDhanuSulistyo@users.noreply.github.com>
2025-05-14 10:22:08 +0700

    fixing package.swift 2

--- package.swift
+++ package.swift
@@ -1,21 +1,42 @@
 // swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "SwiftSocket",
     platforms: [
-        .iOS(.v11)
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v4)
     ],
     products: [
         .library(
             name: "SwiftSocket",
-            targets: ["SwiftSocket"]
-        ),
+            type: .dynamic,
+            targets: ["SwiftSocket", "SwiftSocketC"])
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "SwiftSocket",
-            path: "Sources"
+            dependencies: ["SwiftSocketC"],
+            path: "Sources",
+            exclude: ["ytcpsocket.c"],
+            sources: ["TCPClient.swift", "TCPServer.swift", "UDPClient.swift", "UDPServer.swift", "Byte.swift", "Error.swift", "Result.swift", "Socket.swift"]
+        ),
+        .target(
+            name: "SwiftSocketC",
+            dependencies: [],
+            path: "Sources",
+            sources: ["ytcpsocket.c"],
+            publicHeadersPath: "."
+        ),
+        .testTarget(
+            name: "SwiftSocketTests",
+            dependencies: ["SwiftSocket"],
+            path: "Tests/SwiftSocketTests"
         )
     ]
 )
